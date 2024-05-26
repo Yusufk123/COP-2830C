a845847a3dcc6510f8143a20bb3ef3c169eb6503
Yusufk123 <yusufk1230@gmail.com>
2024-05-26 17:46:29 -0400

    Removed Unneeded import

--- Project 1 copy.lua
+++ Project 1 copy.lua
@@ -1,22 +1,26 @@
 import time
+from xmlrpc.client import boolean
 
-#Scenario: Should I keep drinking coffee today?
+--Scenario: Should I keep drinking coffee today?
 
-#If it's before 3 pm, I've had less than 3 cups of coffee already, and I have a busy day ahead, then I can keep drinking coffee today. Otherwise, I should stop drinking any more coffee today.
+--If it's before 3 pm, I've had less than 3 cups of coffee already, and I have a busy day ahead, then I can keep drinking coffee today. Otherwise, I should stop drinking any more coffee today.
 
-#Variables:
+--Variable definition:
+local currentTime, timeFormat, bdtxt, varlist, coffeeDranken, busyday
+
+--Variables Initialization:
 currentTime = time.localtime()
 timeFormat = time.strftime("%I:%M %p", currentTime)
 bdtxt = "is"
 varlist = ["coffeeDranken", "busyday", "bdtxt"]
 
-#User Inputs
+--User Inputs
 coffeeDranken = int(input("How many coffee have you drank today? "))
 busyday = bool(input("Is Today is a busy day? Leave Blank for No: "))
 
 print(varlist)
 
-#Code:
+--Code:
 def Code(coffeeDranken, busyday, bdtxt):
     if  busyday != True:
         bdtxt = "isn't"
@@ -26,7 +30,7 @@ def Code(coffeeDranken, busyday, bdtxt):
     else:
         print("Ive had", coffeeDranken, "cups of coffee already, its", timeFormat, "and there", bdtxt, "a busy day ahead, So I shouldn't keep drinking coffee today.\n")
         
-#Call Function
+--Call Function
 Code(coffeeDranken, busyday, bdtxt)
 
-#End of Program
+--End of Program
