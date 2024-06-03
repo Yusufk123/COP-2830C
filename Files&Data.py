3b550dda0b4262ea0c0fe06df2fa30857cd6de09
Yusufk123 <yusufk1230@gmail.com>
2024-06-02 23:31:34 -0400

    Touched up the look of fig

--- Files&Data.py
+++ Files&Data.py
@@ -5,5 +5,5 @@ import plotly.express as px
 data = pd.read_csv('gps_day.csv')
 
 #Display Data
-fig = px.scatter(data, x="28.688875833", y="-81.515212833", title="GPS Data")
+fig = px.scatter(data, x="28.688875833", y="-81.515212833", color='track:0.0000', title="GPS Data")
 fig.show()
